6230f50f07bb29eba675c12fd4659e25c03103f3
hasibmuhammad <hasibmu99@gmail.com>
2024-06-23 16:32:14 +0600

    Usermodel and videomodel done

--- src/utils/asyncHanlder.js
+++ src/utils/asyncHanlder.js
@@ -1,5 +1,6 @@
-export const asyncHandler = (fn) => {
+const asyncHandler = (fn) => {
   (req, res, next) => {
-    Promise.resolve(fn(req, res, next)).catch((error) => next(error));
+    Promise.resolve(fn(req, res, next)).catch((error) => fn(error));
   };
 };
+export { asyncHandler };
