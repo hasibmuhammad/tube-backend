d9d6ba67fa6d1a1a5118c42360598dee92955ded
hasibmuhammad <hasibmu99@gmail.com>
2024-06-23 15:23:00 +0600

    API response utils added

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -6,10 +6,10 @@ class ApiError extends Error {
     stack = ""
   ) {
     super(message);
-    this.data = null;
     this.statusCode = statusCode;
     this.message = message;
     this.errors = errors;
+    this.data = null;
     this.success = false;
 
     if (stack) {
@@ -19,3 +19,25 @@ class ApiError extends Error {
     }
   }
 }
+
+// class ApiError extends Error {
+//   constructor(
+//     statusCode,
+//     message = "Something Went Wrong",
+//     errors = [],
+//     stack = ""
+//   ) {
+//     super(message);
+//     this.data = null;
+//     this.statusCode = statusCode;
+//     this.message = message;
+//     this.errors = errors;
+//     this.success = false;
+
+//     if (stack) {
+//       this.stack = stack;
+//     } else {
+//       Error.captureStackTrace(this, this.constructor);
+//     }
+//   }
+// }
--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -1,8 +1,17 @@
 class ApiResponse {
-  constructor(statusCode, data, message = "Success") {
+  constructor(statusCode, message = "Success", data) {
     this.statusCode = statusCode;
-    this.data = data;
     this.message = message;
+    this.data = data;
     this.success = statusCode < 400;
   }
 }
+
+// class ApiResponse {
+//   constructor(statusCode, data, message = "Success") {
+//     this.statusCode = statusCode;
+//     this.data = data;
+//     this.message = message;
+//     this.success = statusCode < 400;
+//   }
+// }
