13997ccaf13e7238061afb0666f3a36122ab0fe7
hasibmuhammad <hasibmu99@gmail.com>
2024-06-23 15:16:44 +0600

    Async Handler re-written

--- src/utils/asyncHanlder.js
+++ src/utils/asyncHanlder.js
@@ -1,6 +1,5 @@
-const asyncHandler = (fn) => {
+export const asyncHandler = (fn) => {
   (req, res, next) => {
-    Promise.resolve(fn(req, res, next)).catch((error) => fn(error));
+    Promise.resolve(fn(req, res, next)).catch((error) => next(error));
   };
 };
-export { asyncHandler };
