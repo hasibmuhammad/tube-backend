f62dbe6b26c8ce0a6fd952b84249c68d8ff85ffe
hasibmuhammad <hasibmu99@gmail.com>
2024-06-23 18:18:09 +0600

    Multer configuration - done

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -51,12 +51,14 @@ const userSchema = new Schema(
   { timestamps: true }
 );
 
+// just before the saving - encrypt the password
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
   this.password = bcrypt.hash(this.password, 10);
   next();
 });
 
+// Verify the password
 userSchema.methods.isPasswordCorrect = async function (password) {
   return await bcrypt.compare(password, this.password);
 };
